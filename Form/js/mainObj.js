9e4c1bd4a16081516bf1dab52e616c45f8a3b9a6
answer261 <answer261@gmail.com>
2013-12-04 18:48:40 +0200

    refactoring and added show/hide functions

--- Form/js/mainObj.js
+++ Form/js/mainObj.js
@@ -1,114 +1,123 @@
-var FormObject = {};
-
+var FormObject = {
 /**
  * 
  * @param form - html object.
  */
-FormObject.init = function (form) {
-	this.bindEvents(form);
+	init: function (form) {
+		this.form = form;
 
-	this.showForm(form);
+		this.bindEvents();
+		this.showForm();
+	},
 
-}
-/**
- * 
- * @param form - html object.
- */
-FormObject.showForm = function (form) {
-	form.fadeIn("slow");
+	showForm: function () {
+		this.form.fadeIn("slow");
 
-	if (FormObject.browser("webkit")) {
-		$(":input[type='color']").addClass("input-color");
-	}
-}
-/**
- * 
- * @param form - html object.
- */
-FormObject.bindEvents = function (form) {
-	form.on("submit", function(event){
-		event.preventDefault();
-		FormObject.submitForm(form);
-	});
-}
+		if (this.browser("webkit")) {
+			$(":input[type='color']").addClass("input-color");
+		}
+	},
 
-/**
- * 
- * @param form - html object.
- */
-FormObject.submitForm = function(form){
-	if (FormObject.validator(form)) {
-		var status = {};
+	bindEvents: function () {
+		var that = this;
+		this.form.on("submit", function(event){
+			event.preventDefault();
+			that.submitForm();
+		});
 
-		$.ajax({
-			type	 : form.attr("method"),
-			url 	 : form.attr("action"),
-			data	 : form.serialize(),
-			dateType : "json",
-			async	 : false,
-			success  : function(data) {
-				alert(data);
-			},
-			error 	 : function(error){
-				if(error.status == "404"){
-					status.msg = "File not found";
-					status.class = "errorMsg";
+		$("#close").on("click", function(){
+			that.closeForm();
+		});
+
+		$("#show").on("click", function(){
+			that.showForm();
+			$(this).hide();
+		})
+	},
+
+	closeForm: function () {
+		this.form.hide();
+		this.appendShowButton();
+	},
+
+	submitForm: function(){
+		if (this.validator()) {
+			var status = {};
+
+			$.ajax({
+				type	 : this.form.attr("method"),
+				url 	 : this.form.attr("action"),
+				data	 : this.form.serialize(),
+				dateType : "json",
+				async	 : false,
+				success  : function(data) {
+					alert(data);
+				},
+				error 	 : function(error){
+					if(error.status == "404"){
+						status.msg = "File not found";
+						status.class = "errorMsg";
+					}
 				}
+
+			}).fail(function(){
+				status.msg = "Something wrong heppened";
+				status.class = "errorMsg";
+			});
+
+			this.showStatus(status);
+		}
+
+	},
+
+	validator: function() {
+
+		//function return boolean value, true if form is valid, fasle if not
+		var valid = true,
+			invalidInputs = [],
+			status = {};
+
+			$( this.form + ":input" ).each(function(){
+				var inputLabels = $(this).parent().find("label").html();
+
+				if($.trim(this.value) == ""){
+					valid = false;
+					invalidInputs.push(inputLabels);
+				}
+			});
+
+			if( invalidInputs.length > 0 ){
+				status.msg = "Fields <p>" + invalidInputs.join(", ") + "</p> must not be empty";
+				status.class = "errorMsg";
+			}
+			else{
+				status.msg = "Your data was successfully send";
+				status.class = "successMsg";
 			}
 
-		}).fail(function(){
-			status.msg = "Something wrong heppened";
-			status.class = "errorMsg";
-		});
+			this.showStatus(status);
 
-		FormObject.showStatus(status);
+			return valid;
+	},
+
+	/**
+	 * function showStatus shows form status on the page
+	 *
+	 * @param status - associative array.
+	 */
+	showStatus: function(status) {
+		$("#msg").attr("class", status["class"])
+				 .html(status["msg"]);
+	},
+
+	appendShowButton: function () {
+		$("#show").fadeIn("slow");
+	},
+
+	browser: function (browserName) {
+		return !!$.browser[browserName];
 	}
-
-}
-
-FormObject.validator = function(form) {
-
-	//function return boolean value, true if form is valid, fasle if not
-	var valid = true,
-		invalidInputs = [],
-		status = {};
-
-		$( form + ":input" ).each(function(){
-			var inputLabels = $(this).parent().find("label").html();
-
-			if($.trim(this.value) == ""){
-				valid = false;
-				invalidInputs.push(inputLabels);
-			}
-		});
-
-		if( invalidInputs.length > 0 ){
-			status.msg = "Fields <p>" + invalidInputs.join(", ") + "</p> must not be empty";
-			status.class = "errorMsg";
-		}
-		else{
-			status.msg = "Your data was successfully send";
-			status.class = "successMsg";
-		}
-
-		FormObject.showStatus(status);
-
-		return valid;
-}
-
-/**
- * function showStatus shows form status on the page
- *
- * @param status - associative array.
- */
-FormObject.showStatus = function(status) {
-	$("#msg").attr("class", status["class"])
-			 .html(status["msg"]);
-}
-
-FormObject.browser = function (browserName) {
-	return !!$.browser[browserName];
-}
+};
 
 
 $(document).ready(function() {
